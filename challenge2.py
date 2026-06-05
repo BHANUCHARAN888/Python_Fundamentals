9102645c94631b678a181e3785093dd3dcbb76ae
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-06-05 23:18:55 +0530

    Day 2: Python fundamentals

---
+++ challenge2.py
@@ -0,0 +1,49 @@
+## Check whether a number is positive or negative.
+# num = int(input("Enter number: "))
+# if num > 0:
+#     print("Positive")
+# elif num < 0:
+#     print("Negative")
+# else:
+#     print("zero")
+
+## Check whether a number is even or odd.
+# num = int(input("Enter number: "))
+# if num % 2 == 0:
+#     print("Even number.")
+# else:
+#     print("Odd number.")
+
+## Input marks and print:
+# A (90–100)
+# B (70–89)
+# C (50–69)
+# Fail (<50)
+# marks = int(input("Enter marks: "))
+# if marks >= 90 and marks <= 100:
+#     print("Grade A")
+# elif marks >= 70 and marks < 90:
+#     print("Grade B")
+# elif marks >= 50 and marks < 70:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+## Check if a user can vote (age ≥ 18).
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("Right to vote.")
+# else:
+#     print("not Eligible")
+
+### Write a program that asks for a username and password:
+# username = input("Username: ")
+# password = input("Password: ")
+
+# if username == "admin":
+#     if password == "1234":
+#         print("Login successful")
+#     else:
+#         print("Wrong password")
+# else:
+#     print("User not found")
---
+++ control_flows.py
@@ -0,0 +1,46 @@
+# Control flows
+
+## if statement
+# age = 18
+# if age >= 18:
+#     print("You are an adult")
+
+## if else statement
+# age = 15
+# if age >= 18:
+#     print("You are an adult")
+# else:
+#     print("You are a minor")
+
+## if, elif, else
+# 1
+# age = int(input("Enter your age: "))
+# if age > 13 and age < 19:
+#     print("Teenager")
+# elif age >= 20 and age < 60:
+#     print("Adult")
+# else:
+#     print("You are a senior citizen.")
+
+# 2
+# marks = int(input("Enter the marks: "))
+# if marks >= 90:
+#     print("Grade A")
+# elif marks < 90 and marks > 80:
+#     print("Grade B")
+# elif marks <= 80 and marks >= 75:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+## Nested Condition
+# age = 20
+# has_id = True
+
+# if age >= 18:
+#     if has_id:
+#         print("Entry allowed")
+#     else:
+#         print("Bring your ID")
+# else:
+#     print("Too young")
