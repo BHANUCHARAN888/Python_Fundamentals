153a8dec0a2dcc5620c04efccfeee3008dd48ca9
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-06-11 18:16:41 +0530

    Practice readline and readlines

---
+++ File_Handling/readline_s_file.py
@@ -0,0 +1,35 @@
+# Readline --> which returns a single line in the file
+# while True:
+#     print("1. Add")
+#     print("2. exit")
+#     choice = input("Enter choice: ")
+#     if choice == "1":
+#         name = input("Enter name: ")
+#         with open("name.txt", "a") as file:
+#              file.write(name+"\n")
+#     elif choice == "2":
+#         break
+#     else:
+#         print("Invalid")
+# Data created------->
+#----------------------------------------
+# with open("name.txt", "r") as file:
+#     one = file.readline()
+#     two = file.readline()
+#     three = file.readline()
+#     four = file.readline()
+#     five = file.readline()
+#     six = file.readline()
+
+# print(one)
+# print(two)
+# print(three)
+# print(four)
+# print(five)
+# print(six)
+#----------------------------------------
+# Readlines --> which returns the list of lines in the file
+# with open("name.txt", "r") as file:
+#     data = file.readlines()
+
+# print(data)
