2db3d7aeb50a588844d4df5bdb14eee3cef3b532
BHANUCHARAN888 <bhanucharankalla8523@gmail.com>
2026-06-12 11:40:48 +0530

    Day 8: About OOP

---
+++ OOP/WHY_OOP.py
@@ -0,0 +1,24 @@
+# Before OOP, we mainly used Procedural Programming.
+
+# Procedural programming consists of:
+# --> Variables
+# --> Functions
+# --> Function calls
+
+# This approach works well for smaller applications.
+
+# But when applications become larger:
+# --> Data and functions become separated
+# --> Managing code becomes difficult
+# --> Complexity increases
+# --> Code reusability decreases
+# --> Making changes becomes harder
+
+# OOP solves these problems by combining:
+
+# DATA + FUNCTIONS = OBJECT
+
+# Classes act as blueprints to create objects
+# Objects represent real-world entities with:
+# --> Attributes (data)
+# --> Methods (functions)
